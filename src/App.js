32994b66f1234a450588a114b5a32ecedfeebb03
luis Ramirez <l.r.32@hotmail.com>
2020-07-08 14:59:04 -0700

    added esLint to project

---
+++ src/App.js
@@ -0,0 +1,30 @@
+const Pet = ({ name, animal, breed }) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, name),
+        React.createElement("h2", {}, animal),
+        React.createElement("h2", {}, breed),
+    ]);
+};
+
+const App = () => {
+    return React.createElement("div", { id: "something-important" }, [
+        React.createElement("h1", {}, "Adopt Me!"),
+        React.createElement(Pet, {
+            name: "Luna",
+            animal: "Dog",
+            breed: "German Shepherd",
+        }),
+        React.createElement(Pet, {
+            name: "Pearl",
+            animal: "Dog",
+            breed: "Black Labrador Retriever",
+        }),
+        React.createElement(Pet, {
+            name: "Phoenix",
+            animal: "Dog",
+            breed: "Mixed",
+        }),
+    ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
